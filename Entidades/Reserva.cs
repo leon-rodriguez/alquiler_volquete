76007de0376cc1bfa5dee26e5a1679e2276b7508
leon rodriguez <leonrodriguez71@gmail.com>
2023-11-16 15:08:49 -0300

    pequeños cambios en entidades

---
+++ Entidades/Reserva.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Reserva
+    {
+        private int id;
+        private DateTime fechaInicio;
+        private DateTime fechaFinal;
+        private int clienteId;
+        public Reserva(int id, DateTime fechaInicio, DateTime fechaFinal, int clienteId)
+        {
+            this.id = id;
+            this.fechaInicio = fechaInicio;
+            this.fechaFinal = fechaFinal;
+            this.clienteId = clienteId;
+        }
+    }
+
+
+}
